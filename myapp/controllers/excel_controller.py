cccf1ff2f017c4de1fdd8767984e5ca7a27f38b5
Santiago Alvarez <salvarezd3@eafit.edu.co>
2024-11-22 01:35:05 -0500

    Trying fixing errors

--- myapp/controllers/excel_controller.py
+++ myapp/controllers/excel_controller.py
@@ -8,15 +8,10 @@ OUTPUT_PATH = os.path.join(os.getcwd(), 'plantilla_modificada.xlsx')
 def rellenar_excel(request):
     data = request.json
     try:
-        excel_buffer = procesar_excel(data)
-        return send_file(
-            excel_buffer,
-            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-            as_attachment=True,
-            download_name='plantilla_modificada.xlsx'
-        )
+        procesar_excel(data)
+        return send_file(OUTPUT_PATH, as_attachment=True, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     except FileNotFoundError:
-        return "El archivo de plantilla de Excel no se encontró.", 404
+        return "El archivo de plantilla de Excel no se encontró. Verifique la ruta. controller", 404
     except Exception as e:
         return str(e), 500
     
